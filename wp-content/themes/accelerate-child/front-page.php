bf875d12abc14aa9f7357a7e42cd6adcb20216c3
Tameka Evans <therealtameka@gmail.com>
2016-01-29 11:47:55 -0500

    modified css and front page files

--- wp-content/themes/accelerate-child/front-page.php
+++ wp-content/themes/accelerate-child/front-page.php
@@ -13,6 +13,7 @@
  */
 
 get_header(); ?>
+
 <section class="home-page">
 	<div class="site-content">
 		<?php while ( have_posts() ) : the_post(); ?>
@@ -20,27 +21,22 @@ get_header(); ?>
 				<?php the_content(); ?>
 				<a class="button" href="<?php echo home_url(); ?>/blog">View Our Work</a>
 			</div>
-			<section class="recent-posts">
-	           <div class="site-content">
-		         <div class="blog-post">
-                    <h4>From the Blog</h4>
-                    <?php query_posts('posts_per_page=1'); ?>
-                    <?php while ( have_posts() ) : the_post(); ?>
-                    <h4><?php the_title(); ?></h4>
+		<?php endwhile; // end of the loop. ?>
+	</div><!-- .site-content -->
+</section><!-- .home-page -->
+
+<section class="recent-posts">
+	<div class="site-content">
+		<div class="blog-post">
+			<h4>From the Blog</h4>
+			<?php query_posts('posts_per_page=1'); ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<h2><?php the_title(); ?></h2>
 				<?php the_excerpt(); ?>
 				<a href="<?php the_permalink(); ?>" class="read-more-link">Read More <span>&rsaquo;</span></a>
 			<?php endwhile; // end of the loop. ?>
 			<?php wp_reset_query(); // resets the altered query back to the original ?>
-
 		</div>
-	</div>
-</section>
-		<?php endwhile; // end of the loop. ?>
-	</div><!-- .container -->
-</section><!-- .home-page -->
-<section class="recent-posts">
-	<div class="site-content">
- 
 	</div>
 </section>
 
